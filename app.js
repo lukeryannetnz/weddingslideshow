b6c7fe6740f7238d365b38c43bdeb5b30ac98690
Luke Ryan <hello@lukeryan.net.nz>
2015-03-04 21:13:06 +1300

    only reset hash if it was set

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ $(document).ready(function(){
     window.location = "https://instagram.com/oauth/authorize/?client_id=834dd36a346648bdb999084cd10c3c79&redirect_uri=http://weddingslideshow.azurewebsites.net&response_type=token"
   })
 
-  var accessToken = location.hash.split('=')[1];
-  console.log('access token'.concat(accessToken));
-  location.hash = null;
+  if(location.hash)
+  {
+    var accessToken = location.hash.split('=')[1];
+    console.log('access token'.concat(accessToken));
+    location.hash = '';
+  }
 })
