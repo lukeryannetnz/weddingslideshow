06d662639f67a1947a5087cab4698f03cded9488
Luke Ryan <hello@lukeryan.net.nz>
2015-03-04 21:20:52 +1300

    starting to construct query for images;

--- app.js
+++ app.js
@@ -3,10 +3,19 @@ $(document).ready(function(){
     window.location = "https://instagram.com/oauth/authorize/?client_id=834dd36a346648bdb999084cd10c3c79&redirect_uri=http://weddingslideshow.azurewebsites.net&response_type=token"
   })
 
+  var accessToken;
+
   if(location.hash)
   {
-    var accessToken = location.hash.split('=')[1];
-    console.log('access token'.concat(accessToken));
+    accessToken = location.hash.split('=')[1];
+    console.log('access token: '.concat(accessToken));
     location.hash = '';
   }
+
+  $("#go").click(function(){
+    var tag = ("#hashtag").val();
+    var searchuri = "https://api.instagram.com/v1/tags/" + tag + "/media/recent?access_token=" + accessToken;
+    console.log('searchuri');
+    $().ajax("searchuri")
+  })
 })
