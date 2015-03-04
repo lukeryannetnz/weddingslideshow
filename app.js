2a40939cede5805034463ab1d81ff6f282cefd90
Luke Ryan <hello@lukeryan.net.nz>
2015-03-04 21:02:41 +1300

    added js to redirect to instagram

---
+++ app.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+  $("#login").click(function(){
+    window.location = "https://instagram.com/oauth/authorize/?client_id=834dd36a346648bdb999084cd10c3c79&redirect_uri=http://weddingslideshow.azurewebsites.net/loginconfirm&response_type=token"
+  })
+})
