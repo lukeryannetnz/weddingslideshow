01a9dec1458bfe9ac21402d6499000f54e9a189b
Luke Ryan <hello@lukeryan.net.nz>
2015-03-05 00:04:54 +1300

    fullscreen

--- app.js
+++ app.js
@@ -27,6 +27,8 @@ function LoadImages(imageData)
           }
       }
   })
+
+  FullscreenImage();
 }
 
 function RedirectToLogin()
@@ -69,6 +71,37 @@ function UpdateImageSrc()
     var index = getRandomInt(0, imageData.imageUris.length - 1);
     $("#photo").attr("src",imageData.imageUris[index]);
   }
+}
+
+function FullscreenImage()
+{
+  var $img = $('#photo'),
+  imageWidth = $img[0].width, //need the raw width due to a jquery bug that affects chrome
+  imageHeight = $img[0].height, //need the raw height due to a jquery bug that affects chrome
+  maxWidth = $(window).width(),
+  maxHeight = $(window).height(),
+  widthRatio = maxWidth / imageWidth,
+  heightRatio = maxHeight / imageHeight;
+
+  var ratio = widthRatio; //default to the width ratio until proven wrong
+
+  if (widthRatio * imageHeight > maxHeight) {
+    ratio = heightRatio;
+  }
+
+  //now resize the image relative to the ratio
+  $img.attr('width', imageWidth * ratio)
+  .attr('height', imageHeight * ratio);
+
+  //and center the image vertically and horizontally
+  $img.css({
+    margin: 'auto',
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0
+  });
 }
 
 $(document).ready(function(){
