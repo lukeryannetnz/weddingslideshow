367a1dcc31560742c69966d0acf6162d3582306e
Luke Ryan <hello@lukeryan.net.nz>
2015-03-04 22:08:58 +1300

    refactor to smaller methods

--- app.js
+++ app.js
@@ -1,27 +1,44 @@
 $(document).ready(function(){
-  $("#login").click(function(){
-    window.location = "https://instagram.com/oauth/authorize/?client_id=834dd36a346648bdb999084cd10c3c79&redirect_uri=http://weddingslideshow.azurewebsites.net&response_type=token"
-  })
+  $("#login").click(RedirectToLogin);
+  $("#go").click(LoadImages)
 
-  var accessToken;
+  var accessToken = TryGetAccessToken;
+  if(!accessToken)
+  {
+    ShowLoginButton();
+  }
+})
+
+function LoadImages()
+{
+  var tag = $("#hashtag").val();
+  var searchuri = "https://api.instagram.com/v1/tags/" + tag + "/media/recent?access_token=" + accessToken + "&callback=?";
+  console.log(searchuri);
+  $.getJSON(searchuri, function(response){
+    console.log(response);
+  })
+}
+
+function RedirectToLogin()
+{
+  window.location = "https://instagram.com/oauth/authorize/?client_id=834dd36a346648bdb999084cd10c3c79&redirect_uri=http://weddingslideshow.azurewebsites.net&response_type=token"
+}
+
+function TryGetAccessToken()
+{
+  var token;
 
   if(location.hash)
   {
-    accessToken = location.hash.split('=')[1];
+    token = location.hash.split('=')[1];
     console.log('access token: '.concat(accessToken));
     location.hash = '';
   }
-  else
-  {
-    $("#login").removeClass("hidden");
-  }
 
-  $("#go").click(function(){
-    var tag = $("#hashtag").val();
-    var searchuri = "https://api.instagram.com/v1/tags/" + tag + "/media/recent?access_token=" + accessToken + "&callback=?";
-    console.log(searchuri);
-    $.getJSON(searchuri, function(response){
-          console.log(response);
-    })
-  })
-})
+  return token;
+}
+
+function ShowLoginButton()
+{
+  $("#login").removeClass("hidden");
+}
