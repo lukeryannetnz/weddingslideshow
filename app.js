ff32f4c7c346076c38d449c2dbee61bc10b9c9ce
Luke Ryan <hello@lukeryan.net.nz>
2015-03-17 21:58:49 +1300

    increasing timeout

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ function GoButtonHander(imageData){
 
   imageData.data.LoadImages(tag, 50);
   window.setInterval(imageData.data.LoadImages, DataPollFrequency, tag, 10)
-  window.setTimeout(UpdateImageSrc, 500, imageData.data);
+  window.setTimeout(UpdateImageSrc, 800, imageData.data);
   window.setInterval(UpdateImageSrc, ImageSwapFrequency, imageData.data)
   FullscreenImage();
   $(window).resize(FullscreenImage);
