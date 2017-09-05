ec856934bb27d6514e7a21d78b0c7e9b0952f75c
lukeryannetnz <github@lukeryan.net.nz>
2017-09-04 19:16:07 -0700

    Documentation for Flickr service.

--- weddingslideshow.api/weddingslideshow.api/DataAccess/IFlickrService.cs
+++ weddingslideshow.api/weddingslideshow.api/DataAccess/IFlickrService.cs
@@ -3,6 +3,10 @@ using Microsoft.Extensions.Configuration;
 
 namespace weddingslideshow.api.DataAccess
 {
+    /// <summary>
+    /// Communicates with the Flickr API.
+    /// <seealso href="https://www.flickr.com/services/api/">The Flicker API documentation.</seealso>
+    /// </summary>
     public interface IFlickrService
     {
       
